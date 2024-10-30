7e3820b94664d1ea36edbd7536bffbccce89b8aa
chenjy <chenjy@chenjydeMacBook-Air.local>
2024-10-30 11:20:36 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  // components: {
+
+  // },
+});
+</script>
+
+<!-- <style>
+
+</style> -->
